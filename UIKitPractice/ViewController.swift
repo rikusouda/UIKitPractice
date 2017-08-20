5f836a39a38d9bb5d734ec61d38cd95474f804ed
Yuki Yoshioka <rikusouda@users.noreply.github.com>
2017-08-20 17:31:22 +0900

    Initial Commit

---
+++ UIKitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKitPractice
+//
+//  Created by Yuki Yoshioka on 2017/08/20.
+//  Copyright © 2017年 rikusouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
